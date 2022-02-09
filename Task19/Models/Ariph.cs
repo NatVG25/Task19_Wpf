cfbc4be8da53f4e13145625b153fea262f1e550e
nvg25 <nvg25@bk.ru>
2022-02-09 23:27:35 +0500

    Добавьте файлы проекта.

---
+++ Task19/Models/Ariph.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task19.Models
+{
+    static class Ariph
+    {
+        public static double Add(double r)
+        {
+            double l = 2 * Math.PI * r;
+            return l;
+        }
+    }
+}
---
+++ Task19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Task19.Models;
+
+namespace Task19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+       
+        private double lenght;
+        public double Lenght
+        {
+            get => lenght;
+            set
+            {
+                lenght = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecuted(object p) //метод, который должен выполнять вычисление
+        //длины окружности и результут присваивать в поле Lenght
+        {
+            Lenght = Ariph.Add(Radius);
+        }
+        private bool CanAddCommandExecuted(object p) //метод проверяющий возможность выполнения команды вычисления
+        {
+            if (Radius > 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel() //конструктор
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecuted, CanAddCommandExecuted);
+        }
+    }
+}
+
